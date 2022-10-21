3aa6a2707e97cc44a616cf48cdf16e6ee64c1997
Guilherme Henrique Minuzzo <108365353+guizn12@users.noreply.github.com>
2022-10-21 16:01:18 -0300

    Add files via upload

---
+++ Projeto Entra21/html/Script/Script_Log.js
@@ -0,0 +1,10 @@
+
+  
+  var modal = document.getElementById('id01');
+  
+  // clique
+  window.onclick = function(event) {
+      if (event.target == modal) {
+          modal.style.display = "none";
+      }
+  }
